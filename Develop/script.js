13c5a0bbb6d2f65dfe08a1dcbe89de56b935585e
edgyhyeji <153347353+edgyhyeji@users.noreply.github.com>
2023-12-22 15:49:13 -0600

    commit test bc it's someone else's starter code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,21 @@
 // Assignment code here
+//click button to generate a password
 
+//when prompted for length of password, i choose length between 8-128
 
+//when asked for character types, i confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+//when i answer each prompt, my input should be validated and at least one character type should be selected.
+
+//when all prompts are answered, a password is generated based on selected criteria
+
+//when password is generated, password is displayed in an alert or written to the page.
+const lengthOfPass = prompt("Enter the password length (between 8 and 128 characters).")
+const chosenLengthOfPass = 8 < lengthOfPass <= 128 
+
+if(chosenLengthOfPass) {
+  
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
